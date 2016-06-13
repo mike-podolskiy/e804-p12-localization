b38cbdc2ed905e2a58b825b2d6f75c23dcd81a1a
Михаил Подольский <podolskiy.mikhail@inessoft.kz>
2016-06-13 08:41:14 +0600

    DateTime

---
+++ src/main/java/kz/mix/e804/localization/dateformat/DateStyleFormats.java
@@ -0,0 +1,21 @@
+//CHECKSTYLE:OFF
+package kz.mix.e804.localization.dateformat;
+
+import java.util.*;
+import java.text.*;
+
+// Demonstrates the use of constants in DateFormat that determines the display style
+public class DateStyleFormats {
+    public static void main(String[] args) {
+        Date now = new Date();
+        Locale.setDefault(Locale.US);
+        int[] dateStyleFormats = { DateFormat.SHORT, DateFormat.MEDIUM, DateFormat.LONG, DateFormat.FULL, DateFormat.DEFAULT };
+        System.out.println("Today's date in different styles are: ");
+        // print today's date in all four formats plus
+        // the default format in the default Locale
+        for (int dateStyleFormat : dateStyleFormats) {
+            DateFormat dateFormat = DateFormat.getDateInstance(dateStyleFormat);
+            System.out.println(dateFormat.format(now));
+        }
+    }
+}
